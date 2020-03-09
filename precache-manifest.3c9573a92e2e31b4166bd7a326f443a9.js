6ccdcd7245f4c7772d86e824f84eeab4d29b5798
Greg Murray <44830296+gmmurray@users.noreply.github.com>
2020-03-09 15:56:54 -0400

    Updates

---
+++ precache-manifest.3c9573a92e2e31b4166bd7a326f443a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33954b7dbcea0a7823d7d8caa239f40b",
+    "url": "/react-blood-donor-form-tutorial/index.html"
+  },
+  {
+    "revision": "594fee36c31d81cddcd1",
+    "url": "/react-blood-donor-form-tutorial/static/css/main.24f14a89.chunk.css"
+  },
+  {
+    "revision": "07fbd2cb6634599e2498",
+    "url": "/react-blood-donor-form-tutorial/static/js/2.a0a5693f.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/react-blood-donor-form-tutorial/static/js/2.a0a5693f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "594fee36c31d81cddcd1",
+    "url": "/react-blood-donor-form-tutorial/static/js/main.93b109d2.chunk.js"
+  },
+  {
+    "revision": "16cc7ab860b2d6d21a10",
+    "url": "/react-blood-donor-form-tutorial/static/js/runtime-main.489a362a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-blood-donor-form-tutorial/static/media/logo.5d5d9eef.svg"
+  }
+]);
